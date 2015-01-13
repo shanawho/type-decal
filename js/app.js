7916eb3dc24900d2007d373bfc503dba6a243460
Shana Hu <shanawho@gmail.com>
2015-01-12 23:41:55 -0800

    lesson1

--- js/app.js
+++ js/app.js
@@ -7,25 +7,65 @@
       templateUrl: "pages/main.html",
       controller: "mainController",
       title: "Working with Typography"
-    }).when("/about", {
-      templateUrl: "pages/lesson.html",
-      controller: "aboutController",
-      title: "About | Working with Typography"
+    }).when("/lessons/1", {
+      templateUrl: "pages/lesson1.html",
+      controller: "lesson1Controller",
+      title: "Lesson 1: Letterforms"
+    }).when("/lessons/2", {
+      templateUrl: "pages/lesson2.html",
+      controller: "lesson2Controller",
+      title: "Lesson 2: Intro to Lettering"
+    }).when("/lessons/3", {
+      templateUrl: "pages/lesson3.html",
+      controller: "lesson3Controller",
+      title: "Lesson 3: Intro to Typography"
+    }).when("/lessons/4", {
+      templateUrl: "pages/lesson4.html",
+      controller: "lesson4Controller",
+      title: "Lesson 4: Typographic Systems"
+    }).when("/lessons/5", {
+      templateUrl: "pages/lesson5.html",
+      controller: "lesson5Controller",
+      title: "Lesson 5: Editorial Design"
+    }).when("/lessons/6", {
+      templateUrl: "pages/lesson6.html",
+      controller: "lesson6Controller",
+      title: "Lesson 6: Typography for Web & Mobile"
+    }).when("/lessons/7", {
+      templateUrl: "pages/lesson7.html",
+      controller: "lesson7Controller",
+      title: "Lesson 7: Identity Design"
+    }).when("/lessons/8", {
+      templateUrl: "pages/lesson8.html",
+      controller: "lesson8Controller",
+      title: "Lesson 8: Spatial Typography"
+    }).when("/lessons/9", {
+      templateUrl: "pages/lesson9.html",
+      controller: "lesson9Controller",
+      title: "Lesson 1: Transmedia Typography"
+    }).when("/lessons/10", {
+      templateUrl: "pages/lesson10.html",
+      controller: "lesson10Controller",
+      title: "Lesson 10: Exploring Concept"
+    }).when("/lessons/11", {
+      templateUrl: "pages/lesson11.html",
+      controller: "lesson11Controller",
+      title: "Lesson 11: Synthesis"
+    }).when("/lessons/11", {
+      templateUrl: "pages/lesson1.html",
+      controller: "lesson1Controller",
+      title: "Lesson 12: Final Critique"
     });
 
   });
-
-  app.run(['$location', '$rootScope', function($location, $rootScope) {
-      $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-          $rootScope.title = current.$$route.title;
-      });
-  }]);
 
   app.controller("navController", function($scope, $location) {
     $scope.isActive = function(viewLocation) {
       return viewLocation === $location.path();
     };
   });
+
+  var lessons;
 
   app.controller("mainController", function($scope) {
 
@@ -44,9 +84,71 @@
       {title: "Final Critique", subtitle: "Final project due",  date: "21 Apr 2014"}
 
     ];
-
+    lessons = $scope.lessons;
 
   });
+
+  app.controller("lesson1Controller", function($scope) {
+    $scope.due = "Bring some snacks to share with the class!";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+
+  app.controller("lesson2Controller", function($scope) {
+    $scope.due = "handmade typography";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson3Controller", function($scope) {
+    $scope.due = "dropcap";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson4Controller", function($scope) {
+    $scope.due = "typeface pairings";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson5Controller", function($scope) {
+    $scope.due = "12 typographic system layouts";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson6Controller", function($scope) {
+    $scope.due = "editorial spread";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson7Controller", function($scope) {
+    $scope.due = "midterm project";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson8Controller", function($scope) {
+    $scope.due = "museum identity";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson9Controller", function($scope) {
+    $scope.due = "gallery design";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson10Controller", function($scope) {
+    $scope.due = "exhibit design";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson11Controller", function($scope) {
+    $scope.due = "design influences culture";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
+  app.controller("lesson12Controller", function($scope) {
+    $scope.due = "Delightful confusion";
+    $scope.title = lessons[0].title;
+    $scope.date = lessons[0].date;
+  })
 
 
 }).call(this);
