55e9635c69a1f2a86794a8e8aceb191426becc4f
Shana Hu <shanawho@gmail.com>
2015-03-02 22:31:38 -0800

    lesson 5

--- js/app.js
+++ js/app.js
@@ -26,23 +26,23 @@
     }).when("/lessons/2", {
       templateUrl: "pages/lesson2.html",
       controller: "lesson2Controller",
-      title: "Lesson 2: Intro to Lettering"
+      title: "Lesson 2: Handlettering”
     }).when("/lessons/3", {
       templateUrl: "pages/lesson3.html",
       controller: "lesson3Controller",
-      title: "Lesson 3: Intro to Typography"
+      title: "Lesson 3: Digital Lettering”
     }).when("/lessons/4", {
       templateUrl: "pages/lesson4.html",
       controller: "lesson4Controller",
-      title: "Lesson 4: Typographic Systems"
+      title: "Lesson 4: Intro to Typography”
     }).when("/lessons/5", {
       templateUrl: "pages/lesson5.html",
       controller: "lesson5Controller",
-      title: "Lesson 5: Editorial Design"
+      title: "Lesson 5: Typographic Hierarchy & Systems”
     }).when("/lessons/6", {
       templateUrl: "pages/lesson6.html",
       controller: "lesson6Controller",
-      title: "Lesson 6: Typography for Web & Mobile"
+      title: "Lesson 6: Editorial Design”
     }).when("/lessons/7", {
       templateUrl: "pages/lesson7.html",
       controller: "lesson7Controller",
@@ -50,19 +50,19 @@
     }).when("/lessons/8", {
       templateUrl: "pages/lesson8.html",
       controller: "lesson8Controller",
-      title: "Lesson 8: Spatial Typography"
+      title: "Lesson 8: Typography for Web & Mobile"
     }).when("/lessons/9", {
       templateUrl: "pages/lesson9.html",
       controller: "lesson9Controller",
-      title: "Lesson 1: Transmedia Typography"
+      title: "Lesson 1: Spatial Typography"
     }).when("/lessons/10", {
       templateUrl: "pages/lesson10.html",
       controller: "lesson10Controller",
-      title: "Lesson 10: Exploring Concept"
+      title: "Lesson 10: Transmedia Typography"
     }).when("/lessons/11", {
       templateUrl: "pages/lesson11.html",
       controller: "lesson11Controller",
-      title: "Lesson 11: Synthesis"
+      title: "Lesson 11: Exploring Concept”
     }).when("/lessons/11", {
       templateUrl: "pages/lesson1.html",
       controller: "lesson1Controller",
@@ -86,19 +86,18 @@
       {title: "Letterforms", date: "03 Feb 2015"},
       {title: "Handlettering", date: "10 Feb 2015"},
       {title: "Digital Lettering", date: "17 Feb 2015"},
-      {title: "Intro to Typography", date: "24 Feb 2015"}
+      {title: "Intro to Typography", date: "24 Feb 2015”},
+      {title: “Typographic Hierarchy & Systems“, date: "03 Mar 2015"}
     ]
 
     $scope.lessons = [
-      {title: "Editorial Design", date: "03 Mar 2015"},
-      {title: "Typography for Web & Mobile", date: "10 Mar 2015"},
-      {title: "Identity Design", date: "17 Mar 2015"},
+      {title: “Editorial Design”, date: "10 Mar 2015"},
+      {title: "Identity Design", date: "17 Mar 2015”, subtitle: “Midterm project due”},
+      {title: “Typography for Web & Mobile“, date: "21 Apr 2015"},
       {title: "Spatial Typography", date: "31 Mar 2015"},
       {title: "Transmedia Typography", date: "07 Apr 2015"},
       {title: "Exploring Concept", date: "14 Apr 2015"},
-      {title: "Creative Workshop", date: "21 Apr 2015"},
       {title: "Final Critique", subtitle: "Final project due",  date: "28 Apr 2015"}
-
     ];
     released = $scope.released;
     lessons = $scope.lessons;
@@ -115,7 +114,6 @@
 
   app.controller("lesson2Controller", function($scope) {
     $scope.week = 2;
-    $scope.due = "handmade typography";
     $scope.title = released[1].title;
     $scope.date = released[1].date;
     $scope.prevLesson = $scope.week-1;
@@ -123,7 +121,6 @@
   })
   app.controller("lesson3Controller", function($scope) {
     $scope.week = 3;
-    $scope.due = "dropcap";
     $scope.title = released[2].title;
     $scope.date = released[2].date;
     $scope.prevLesson = $scope.week-1;
@@ -131,7 +128,6 @@
   })
   app.controller("lesson4Controller", function($scope) {
     $scope.week = 4;
-    $scope.due = "typeface pairings";
     $scope.title = released[3].title;
     $scope.date = released[3].date;
     $scope.prevLesson = $scope.week-1;
@@ -139,7 +135,6 @@
   })
   app.controller("lesson5Controller", function($scope) {
     $scope.week = 5;
-    $scope.due = "12 typographic system layouts";
     $scope.title = released[4].title;
     $scope.date = released[4].date;
     $scope.prevLesson = $scope.week-1;
@@ -147,7 +142,6 @@
   })
   app.controller("lesson6Controller", function($scope) {
     $scope.week = 6;
-    $scope.due = "editorial spread";
     $scope.title = released[5].title;
     $scope.date = released[5].date;
     $scope.prevLesson = $scope.week-1;
@@ -155,7 +149,6 @@
   })
   app.controller("lesson7Controller", function($scope) {
     $scope.week = 7;
-    $scope.due = "midterm project";
     $scope.title = released[6].title;
     $scope.date = released[6].date;
     $scope.prevLesson = $scope.week-1;
@@ -163,7 +156,6 @@
   })
   app.controller("lesson8Controller", function($scope) {
     $scope.week = 8;
-    $scope.due = "museum identity";
     $scope.title = released[7].title;
     $scope.date = released[7].date;
     $scope.prevLesson = $scope.week-1;
@@ -171,7 +163,6 @@
   })
   app.controller("lesson9Controller", function($scope) {
     $scope.week = 9;
-    $scope.due = "gallery design";
     $scope.title = released[8].title;
     $scope.date = released[8].date;
     $scope.prevLesson = $scope.week-1;
@@ -179,7 +170,6 @@
   })
   app.controller("lesson10Controller", function($scope) {
     $scope.week = 10;
-    $scope.due = "exhibit design";
     $scope.title = released[9].title;
     $scope.date = released[9].date;
     $scope.prevLesson = $scope.week-1;
@@ -187,7 +177,6 @@
   })
   app.controller("lesson11Controller", function($scope) {
     $scope.week = 11;
-    $scope.due = "design influences culture";
     $scope.title = released[10].title;
     $scope.date = released[10].date;
     $scope.prevLesson = $scope.week-1;
@@ -195,7 +184,6 @@
   })
   app.controller("lesson12Controller", function($scope) {
     $scope.week = 12;
-    $scope.due = "Delightful confusion";
     $scope.title = released[11].title;
     $scope.date = released[11].date;
     $scope.prevLesson = $scope.week-1;
